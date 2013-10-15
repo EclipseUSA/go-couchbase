6c6e438248bd23f7f839c75eb59e8bcff9169e30
DerekEclipse <derek@eclipse.io>
2013-10-15 12:51:49 -0700

    updated max connections
    to lower load

--- pools.go
+++ pools.go
@@ -225,7 +225,7 @@ func (b *Bucket) refresh() (err error) {
 	for i := range b.connections {
 		b.connections[i] = newConnectionPool(
 			b.VBucketServerMap.ServerList[i],
-			b.authHandler(), 24)
+			b.authHandler(), 8)
 	}
 	return nil
 }
